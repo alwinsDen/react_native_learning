d913b9eb81e50b616f472960ef09f5e8b68572c0
alwinsDen <jsmadesimple@gmail.com>
2022-04-03 11:13:41 +0530

    mat tab nav

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from "react"
 import {NavigationContainer} from "@react-navigation/native";
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {createMaterialBottomTabNavigator} from "@react-navigation/material-bottom-tabs";
 import Comp1 from "./pages/Comp1";
 import Comp2 from "./pages/Comp2";
 import Comp3 from "./pages/Comp3";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 const App = () => {
     return <NavigationContainer
@@ -38,19 +38,26 @@ const App = () => {
             })}
             tabBarOptions={{
                 activeTintColor: "#f0f", //changes color on being active
-                inactiveTintColor:"#000",
+                inactiveTintColor:"#ec1f1f",
                 activeBackgroundColor:"#e8e8e8",
                 inactiveBackgroundColor:"#ffdcdc",
-                showLabel:true,
+                showLabel:false,
                 labelStyle: {
                     fontFamily:"courier",
                     fontSize:14
-                }
+                },
             }}
+            activeColor={"#b0b0b0"}
+            inactiveColor={"#ffffff"}
+            barStyle={{backgroundColor: "#649fad"}}
         >
             <Tab.Screen
                 name={"Screen_A"}
                 component={Comp1}
+                options={{
+                    tabBarBadge: 3,
+                    tabBarColor:"green"
+                }}
             />
             <Tab.Screen
                 name={"Screen_B"}

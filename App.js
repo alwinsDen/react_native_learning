636033a02e511181e13277b100339addc6e0d380
alwinsDen <jsmadesimple@gmail.com>
2022-04-03 10:53:14 +0530

    normal nav bottom tab

--- App.js
+++ App.js
@@ -1,39 +1,67 @@
 import React from "react"
-import {createStackNavigator} from "@react-navigation/stack";
 import {NavigationContainer} from "@react-navigation/native";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import Comp1 from "./pages/Comp1";
 import Comp2 from "./pages/Comp2";
 import Comp3 from "./pages/Comp3";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
-    return <NavigationContainer>
-        <Stack.Navigator
-            // TO remove header globally and other options
+    return <NavigationContainer
+    >
+        <Tab.Navigator
+            screenOptions={({route}) => ({
+                tabBarIcon: ({focused, size, color}) => {
+                    let iconName;
+                    if (route.name === "Screen_A") {
+                        iconName = "book-medical"
+                        color = focused ? "red" : "lightgreen"
+                        size = focused ? 25 : 20
+                    } else if (route.name === "Screen_B") {
+                        iconName = "file-medical-alt"
+                        color = focused ? "red" : "lightgreen"
+                        size = focused ? 25 : 20
+                    } else if (route.name === "Screen_C") {
+                        iconName = "pump-medical"
+                        color = focused ? "red" : "lightgreen"
+                        size = focused ? 25 : 20
+                    }
 
-            // screenOptions={{
-            //     header: () => null
-            // }}
+                    return <FontAwesome5
+                        name={iconName}
+                        color={color}
+                        size={size}
+                    />
+                },
+            })}
+            tabBarOptions={{
+                activeTintColor: "#f0f", //changes color on being active
+                inactiveTintColor:"#000",
+                activeBackgroundColor:"#e8e8e8",
+                inactiveBackgroundColor:"#ffdcdc",
+                showLabel:true,
+                labelStyle: {
+                    fontFamily:"courier",
+                    fontSize:14
+                }
+            }}
         >
-            <Stack.Screen
+            <Tab.Screen
                 name={"Screen_A"}
                 component={Comp1}
-                options={{
-                    // title:"herro there" there are many options,
-                    header: () => null //removes header for Screen_A
-                }}
             />
-            <Stack.Screen
+            <Tab.Screen
                 name={"Screen_B"}
                 component={Comp2}
             />
-            <Stack.Screen
+            <Tab.Screen
                 name={"Screen_C"}
                 component={Comp3}
             >
-            </Stack.Screen>
-        </Stack.Navigator>
+            </Tab.Screen>
+        </Tab.Navigator>
     </NavigationContainer>
 }
 
